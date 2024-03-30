ed7a92441baadc1753c0426095392067d0548f1c
HeskeyML <60342692+HeskeyML@users.noreply.github.com>
2024-03-30 10:35:04 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("openButton").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "block";
+});
+
+document.getElementById("closeButton").addEventListener("click", function() {
+    document.getElementById("popup").style.display = "none";
+});
