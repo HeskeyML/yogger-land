d5d0aec6e2505bb16bdd5d03fae6cdc3432b3779
HeskeyML <60342692+HeskeyML@users.noreply.github.com>
2024-03-31 21:54:46 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,3 +5,20 @@ function openPopup(popupId) {
 function closePopup(popupId) {
     document.getElementById(popupId).style.display = "none";
 }
+
+const musicToggle = document.getElementById('music-toggle');
+const backgroundMusic = document.getElementById('background-music');
+
+let isMusicPlaying = true;
+
+musicToggle.addEventListener('click', function() {
+    if (isMusicPlaying) {
+        backgroundMusic.pause();
+        isMusicPlaying = false;
+        musicToggle.textContent = 'Play Music';
+    } else {
+        backgroundMusic.play();
+        isMusicPlaying = true;
+        musicToggle.textContent = 'Pause Music';
+    }
+});
