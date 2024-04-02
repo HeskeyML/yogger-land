83189ebd32138109179327d021a0b61d9b074087
HeskeyML <60342692+HeskeyML@users.noreply.github.com>
2024-04-02 17:59:12 +0100

    Update music.js

--- music.js
+++ music.js
@@ -22,6 +22,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     if (!localStorage.getItem('musicPaused')) {
         bgMusic.currentTime = parseFloat(musicPosition);
         bgMusic.play();
+        isMusicPlaying = true;
+        musicToggle.textContent = 'Pause Music';
     }
     // Save music state to localStorage when leaving the page
     window.addEventListener('beforeunload', function() {
