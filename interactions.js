0a0385e47b3c865b8e0fc1249fd2ce3ff8cd9453
HeskeyML <60342692+HeskeyML@users.noreply.github.com>
2024-04-05 09:05:04 +0100

    Update interactions.js

--- interactions.js
+++ interactions.js
@@ -5,3 +5,9 @@ if (!goldPieces > 0) {
   }
 
 localStorage.setItem('goldPieces', goldPieces);
+
+
+
+const inventoryDisplayParagraph = document.getElementById('displayParagraph');
+const inventoryDisplayText = "Gold Pieces: " + goldPieces.toString();
+inventoryDisplayParagraph.textContent = displayText;
